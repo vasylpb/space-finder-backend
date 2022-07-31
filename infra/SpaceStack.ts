2b66e7383ad8aab52a51a42ba2ed2a089f2b8c3b
vpukii <vpukii@n-ix.com>
2022-07-31 16:47:17 +0300

    Lambda testing

--- infra/SpaceStack.ts
+++ infra/SpaceStack.ts
@@ -1,14 +1,10 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import {
-  Code,
-  Function as LambdaFunction,
-  Runtime,
-} from "aws-cdk-lib/aws-lambda";
 import { join } from "path";
 import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
 import { GenericTable } from "./GenericTable";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { PolicyStatement } from "aws-cdk-lib/aws-iam";
 
 export class SpaceStack extends Stack {
   private api = new RestApi(this, "SpaceApi");
@@ -17,19 +13,20 @@ export class SpaceStack extends Stack {
   constructor(scope: Construct, id: string, props: StackProps) {
     super(scope, id, props);
 
-    const helloLambda = new LambdaFunction(this, "helloLambda", {
-      runtime: Runtime.NODEJS_16_X,
-      code: Code.fromAsset(join(__dirname, "..", "services")),
-      handler: "hello.main",
-    });
-
     const helloLambdaNode = new NodejsFunction(this, "helloLambdaNode", {
       entry: join(__dirname, "..", "services", "node-lambda", "hello.ts"),
       handler: "handler",
     });
 
-    // Hello Api lambda integration
-    const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+    // Add bucket policy
+    const s3ListPolicy = new PolicyStatement();
+    s3ListPolicy.addActions("s3:ListAllMyBuckets");
+    s3ListPolicy.addResources("*");
+
+    helloLambdaNode.addToRolePolicy(s3ListPolicy);
+
+    // Hello Api lambda integration with gateway
+    const helloLambdaIntegration = new LambdaIntegration(helloLambdaNode);
     const helloLambdaResource = this.api.root.addResource("hello");
     helloLambdaResource.addMethod("GET", helloLambdaIntegration);
   }
